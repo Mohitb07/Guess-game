7b44f44e4a9e0d798322dd734a10767c6cabc236
mohitb07 <bmohit980@gmail.com>
2021-08-18 20:44:01 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Header from './components/Header';
+import Starter from './screens/Starter';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header title="Guess game"/>
+      <Starter/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});

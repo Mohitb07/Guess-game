d13d17a1ccc9329b6155c90c694313890e53811b
mohitb07 <bmohit980@gmail.com>
2021-08-23 20:59:27 +0530

    game over component added and some states

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './components/Header';
+import GameOver from './screens/GameOver';
+import GameScreen from './screens/GameScreen';
 import Starter from './screens/Starter';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState()
+  const [guessRounds, setGuessRounds] = useState(0)
+
+  const newGameHandler = () => {
+    setGuessRounds(0)
+    setUserNumber(null)
+  }
+  
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber)
+  }
+
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds)
+  }
+  
+  let content = <Starter onStartGame={startGameHandler}/>
+
+  if(userNumber && guessRounds <= 0 ){
+    content = <GameScreen userSelectedNumber={userNumber} onGameOver={gameOverHandler}/>
+  } else if (guessRounds > 0){
+    content = <GameOver rounds={guessRounds} onRestart={newGameHandler}/>
+  }else {
+    content
+  }
   return (
     <View style={styles.container}>
       <Header title="Guess game"/>
-      <Starter/>
+      {content}
       <StatusBar style="auto" />
     </View>
 
